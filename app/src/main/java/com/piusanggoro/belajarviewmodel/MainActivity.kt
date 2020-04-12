f4a9acb13d8078f5949e3f20cd834ad933c90515
PiusAnggoro <piusanggoro@gmail.com>
2020-04-12 22:15:17 +0700

    Initial commit

---
+++ app/src/main/java/com/piusanggoro/belajarviewmodel/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.piusanggoro.belajarviewmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: DataViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(DataViewModel::class.java)
+        viewModel.getHitung().observe(this, getHitung)
+
+        btn_hitung.setOnClickListener {
+            hitung(edt_panjang.text.toString().trim(), edt_panjang.text.toString().trim())
+        }
+    }
+
+    fun hitung(panjang: String, lebar: String) {
+        if (panjang.isEmpty() || lebar.isEmpty()) {
+            Toast.makeText(applicationContext,"Data KOSONG",Toast.LENGTH_SHORT).show()
+        } else {
+            viewModel.setHitung(edt_panjang.text.toString().trim(), edt_lebar.text.toString().trim())
+        }
+    }
+
+    val getHitung = Observer<Result> {
+        it.let {
+            tv_result.text = it?.result
+        }
+    }
+}
+
---
+++ app/src/main/java/com/piusanggoro/belajarviewmodel/Result.kt
@@ -0,0 +1,3 @@
+package com.piusanggoro.belajarviewmodel
+
+class Result(var result: String)
